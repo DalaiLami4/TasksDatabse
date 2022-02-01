52a01aafb548537891f532b1ae5f6777e5582089
DalaiLami4 <98631538+DalaiLami4@users.noreply.github.com>
2022-02-01 11:58:04 +0300

    Task4

---
+++ Task4_Creating.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS `task4`;
+USE `task4`;
+
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `Category` int(11) NOT NULL DEFAULT 1,
+  `HourlyRate` double NOT NULL DEFAULT 0,
+  PRIMARY KEY (`Category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `workers` (
+  `WorkerID` int(11) NOT NULL AUTO_INCREMENT,
+  `Category` int(11) NOT NULL DEFAULT 0,
+  `FullName` varchar(50) NOT NULL DEFAULT '0',
+  `PassportID` varchar(50) NOT NULL DEFAULT '0',
+  `DOB` date NOT NULL,
+  `Phone` varchar(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`WorkerID`),
+  KEY `category` (`Category`),
+  CONSTRAINT `category` FOREIGN KEY (`Category`) REFERENCES `categories` (`Category`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `completed_works` (
+  `CompanyName` varchar(50) NOT NULL,
+  `WorkerID` int(11) NOT NULL,
+  `DateOfComplition` date NOT NULL,
+  `HoursWorked` int(11) NOT NULL,
+  PRIMARY KEY (`CompanyName`),
+  KEY `workerid` (`WorkerID`),
+  CONSTRAINT `workerid` FOREIGN KEY (`WorkerID`) REFERENCES `workers` (`WorkerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
---
+++ Task4_Filling.sql
@@ -0,0 +1,16 @@
+INSERT INTO `categories` (`Category`, `HourlyRate`) VALUES
+	(1, 15000),
+	(2, 8500),
+	(3, 5000);
+
+
+INSERT INTO `workers` (`WorkerID`, `Category`, `FullName`, `PassportID`, `DOB`, `Phone`) VALUES
+	(1, 3, 'Kim Chen In', '283487', '1985-05-26', '89507361975'),
+	(2, 1, 'Vladimir Vladimirovich Putin', '98 45868', '1964-09-13', '11111111111'),
+	(3, 2, 'Iosiph Visorionovich Stalin', '78918446', '1995-11-21', '89956324664');
+
+
+INSERT INTO `completed_works` (`CompanyName`, `WorkerID`, `DateOfComplition`, `HoursWorked`) VALUES
+	('Festo', 2, '2022-02-01', 8),
+	('Siemens', 3, '2021-11-27', 4),
+	('Sitno', 1, '2019-06-20', 6);
