498961faace26429ea4f72bcbf94789bda82a223
DalaiLami4 <98631538+DalaiLami4@users.noreply.github.com>
2022-02-03 17:20:35 +0500

    Corrected

--- Task6/Task6_Creating.sql
+++ Task6/Task6_Creating.sql
@@ -1,11 +1,10 @@
 CREATE DATABASE IF NOT EXISTS `task6`;
 USE `task6`;
 
-
 CREATE TABLE IF NOT EXISTS `countries` (
   `CountryID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) NOT NULL DEFAULT '0',
-  `VisaPrice` double NOT NULL DEFAULT 0,
+  `VisaPrice` decimal(20,6) NOT NULL DEFAULT 0.000000,
   PRIMARY KEY (`CountryID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
@@ -21,11 +20,13 @@ CREATE TABLE IF NOT EXISTS `routes` (
 
 
 CREATE TABLE IF NOT EXISTS `sales` (
+  `SaleID` int(11) NOT NULL AUTO_INCREMENT,
   `RouteID` int(11) NOT NULL,
   `Purpose` varchar(50) NOT NULL,
-  `TicketPrice` double NOT NULL DEFAULT 0,
+  `TicketPrice` decimal(20,6) NOT NULL DEFAULT 0.000000,
   `NumberSales` int(11) NOT NULL DEFAULT 0,
   `DateSale` date NOT NULL,
+  PRIMARY KEY (`SaleID`),
   KEY `routeid` (`RouteID`),
   CONSTRAINT `routeid` FOREIGN KEY (`RouteID`) REFERENCES `routes` (`RouteID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
