d6cb8bf22fcfedc46e58893e47061c3818319948
DalaiLami4 <98631538+DalaiLami4@users.noreply.github.com>
2022-02-08 13:00:38 +0500

    Corrected

--- Task8/Task8_Creating.sql
+++ Task8/Task8_Creating.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE IF NOT EXISTS `task8` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `task8`;
+CREATE DATABASE IF NOT EXISTS `task8_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `task8_test`;
 
 CREATE TABLE IF NOT EXISTS `publications` (
   `PublicationID` int(11) NOT NULL AUTO_INCREMENT,
   `PublicationType` varchar(50) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Price` decimal(20,6) NOT NULL DEFAULT 0.000000,
+  `Price` decimal(6,2) NOT NULL DEFAULT 0.000000,
   PRIMARY KEY (`PublicationID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
@@ -19,13 +19,12 @@ CREATE TABLE IF NOT EXISTS `recipients` (
 
 
 CREATE TABLE IF NOT EXISTS `subscriptions` (
-  `SubscriptionID` int(11) NOT NULL AUTO_INCREMENT,
   `RecipientID` int(11) NOT NULL DEFAULT 0,
   `PublicationID` int(11) NOT NULL DEFAULT 0,
   `SubscriptionTerm` int(11) NOT NULL DEFAULT 0,
   `StartMonth` int(11) NOT NULL DEFAULT 0,
   `StartYear` year(4) NOT NULL DEFAULT 0000,
-  PRIMARY KEY (`SubscriptionID`),
+  PRIMARY KEY (`RecipientID`, `PublicationID`),
   KEY `recipientid` (`RecipientID`),
   KEY `publid` (`PublicationID`),
   CONSTRAINT `publid` FOREIGN KEY (`PublicationID`) REFERENCES `publications` (`PublicationID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
