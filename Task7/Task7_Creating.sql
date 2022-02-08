4d8c365186c9420eb36eae658229e79cb4a418a0
DalaiLami4 <98631538+DalaiLami4@users.noreply.github.com>
2022-02-08 12:32:31 +0500

    Corrected

--- Task7/Task7_Creating.sql
+++ Task7/Task7_Creating.sql
@@ -6,30 +6,30 @@ CREATE TABLE IF NOT EXISTS `products` (
   `ProductID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) NOT NULL,
   `Unit` varchar(50) NOT NULL DEFAULT 'Thing',
-  `RetailPrice` decimal(20,6) NOT NULL,
+  `RetailPrice` decimal(6,2) NOT NULL,
   PRIMARY KEY (`ProductID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `sales` (
+  `SaleID` int(11) NOT NULL AUTO_INCREMENT,
+  `ProductID` int(11) NOT NULL DEFAULT 0,
+  `DateOfSale` date NOT NULL,
+  `NumberSold` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`SaleID`),
+  KEY `prodid` (`ProductID`),
+  CONSTRAINT `prodid` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `departments` (
   `DepartmentID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) NOT NULL,
-  `ProductID` int(11) NOT NULL DEFAULT 0,
+  `SaleID` int(11) NOT NULL DEFAULT 0,
   `DepartmentHead` varchar(50) NOT NULL,
   `Phone` varchar(50) NOT NULL,
-  `Volume` int(11) DEFAULT NULL,
+  `SalesVolume` decimal(6,2) DEFAULT NULL,
   PRIMARY KEY (`DepartmentID`),
-  KEY `prodid` (`ProductID`),
-  CONSTRAINT `prodid` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `saleid` (`SaleID`),
+  CONSTRAINT `saleid` FOREIGN KEY (`SaleID`) REFERENCES `sales` (`SaleID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE IF NOT EXISTS `sales` (
-  `SaleID` int(11) NOT NULL AUTO_INCREMENT,
-  `DepartmentID` int(11) NOT NULL DEFAULT 0,
-  `DateOfSale` date NOT NULL,
-  `NumberSold` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`SaleID`),
-  KEY `depid` (`DepartmentID`),
-  CONSTRAINT `depid` FOREIGN KEY (`DepartmentID`) REFERENCES `departments` (`DepartmentID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
--- Task7/Task7_Filling.sql
+++ Task7/Task7_Filling.sql
@@ -1,18 +1,19 @@
 INSERT INTO `products` (`ProductID`, `Name`, `Unit`, `RetailPrice`) VALUES
-	(1, 'Bread', 'Thing', 30.000000),
-	(2, 'Juice', 'Thing', 50.000000),
-	(3, 'Tea', 'Thing', 48.000000),
-	(4, 'Fruit', 'Kg', 13.000000);
+	(1, 'Bread', 'Thing', 25.00),
+	(2, 'Milk', 'Botle', 55.00),
+	(3, 'Sugar', 'Kg', 13.00),
+	(4, 'Juice', 'Botle', 60.00);
+
+  
+INSERT INTO `sales` (`SaleID`, `ProductID`, `DateOfSale`, `NumberSold`) VALUES
+	(1, 1, '2022-01-16', 25),
+	(2, 2, '2022-02-08', 10),
+	(3, 3, '2021-02-25', 16),
+	(4, 4, '2022-02-09', 0);
 
 
-INSERT INTO `departments` (`DepartmentID`, `Name`, `ProductID`, `DepartmentHead`, `Phone`, `Volume`) VALUES
-	(1, 'Juice', 2, 'Ivanov Ivan Ivanovich', '89507361975', 15),
-	(2, 'Bread', 1, 'Petrova Masha Igorevna', '89824561278', 31),
-	(3, 'Tea', 3, 'Nuri', '56465423132', 9),
-	(4, 'Fruit', 4, 'Tyler Derden', '66666666666', 5);
-
-
-INSERT INTO `sales` (`SaleID`, `DepartmentID`, `DateOfSale`, `NumberSold`) VALUES
-	(1, 1, '2022-01-01', 311),
-	(2, 2, '2021-12-02', 895),
-	(3, 3, '2022-02-02', 17);
+INSERT INTO `departments` (`DepartmentID`, `Name`, `SaleID`, `DepartmentHead`, `Phone`, `SalesVolume`) VALUES
+	(1, 'Alpha', 1, 'El Primo', '89507361975', 625.00),
+	(2, 'Beta', 2, 'Amber', '89825561479', 550.00),
+	(3, 'Gamma', 3, 'Larry', '89502364578', 208.00),
+	(4, 'Delta', 4, 'Dima', '89823349249', 0.00);
