973498b5e87790d65883a61c44d6864c86b1b338
DalaiLami4 <98631538+DalaiLami4@users.noreply.github.com>
2022-02-08 14:19:52 +0500

    Add files via upload

---
+++ Task10_Creating.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE IF NOT EXISTS `task10`;
+USE `task10`;
+
+
+CREATE TABLE IF NOT EXISTS `diagnoses` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `doctors` (
+  `FullName` varchar(50) NOT NULL,
+  `Speciality` varchar(50) NOT NULL,
+  `Category` int(11) NOT NULL,
+  PRIMARY KEY (`FullName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `patients` (
+  `MedicalCardID` int(11) NOT NULL AUTO_INCREMENT,
+  `FullName` varchar(50) NOT NULL,
+  `DOB` varchar(50) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `Gender` varchar(50) NOT NULL,
+  `Discount(%)` float DEFAULT 0,
+  PRIMARY KEY (`MedicalCardID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `appointments` (
+  `AppointmentCard` int(11) NOT NULL AUTO_INCREMENT,
+  `Patient` int(11) NOT NULL,
+  `Doctor` varchar(50) NOT NULL,
+  `DateOfVisit` date NOT NULL,
+  `VisitPurpose` varchar(100) NOT NULL,
+  `Diagnosis` int(11) DEFAULT NULL,
+  `Price` decimal(6,2) NOT NULL,
+  PRIMARY KEY (`AppointmentCard`),
+  KEY `doctor` (`Doctor`),
+  KEY `patient` (`Patient`),
+  KEY `diagnosis` (`Diagnosis`),
+  CONSTRAINT `diagnosis` FOREIGN KEY (`Diagnosis`) REFERENCES `diagnoses` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `doctor` FOREIGN KEY (`Doctor`) REFERENCES `doctors` (`FullName`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `patient` FOREIGN KEY (`Patient`) REFERENCES `patients` (`MedicalCardID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
