40050a33ac9d1f916e3cf191f4eed0abb6eaf471
DalaiLami4 <98631538+DalaiLami4@users.noreply.github.com>
2022-02-10 11:11:42 +0500

    Corrected

--- Task10/Task10_Creating.sql
+++ Task10/Task10_Creating.sql
@@ -28,19 +28,27 @@ CREATE TABLE IF NOT EXISTS `patients` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 
+CREATE TABLE IF NOT EXISTS `appointment_cards` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Doctor` varchar(50) NOT NULL,
+  `Time` text NOT NULL,
+  PRIMARY KEY (`ID`) USING BTREE,
+  KEY `doctor` (`Doctor`),
+  CONSTRAINT `doctor` FOREIGN KEY (`Doctor`) REFERENCES `doctors` (`FullName`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+
 CREATE TABLE IF NOT EXISTS `appointments` (
   `AppointmentCard` int(11) NOT NULL AUTO_INCREMENT,
   `Patient` int(11) NOT NULL,
-  `Doctor` varchar(50) NOT NULL,
   `DateOfVisit` date NOT NULL,
   `VisitPurpose` varchar(100) NOT NULL,
   `Diagnosis` int(11) DEFAULT NULL,
   `Price` decimal(6,2) NOT NULL,
-  PRIMARY KEY (`AppointmentCard`),
-  KEY `doctor` (`Doctor`),
+  PRIMARY KEY (`AppointmentCard`,`DateOfVisit`) USING BTREE,
   KEY `patient` (`Patient`),
   KEY `diagnosis` (`Diagnosis`),
+  CONSTRAINT `appcardid` FOREIGN KEY (`AppointmentCard`) REFERENCES `appointment_cards` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `diagnosis` FOREIGN KEY (`Diagnosis`) REFERENCES `diagnoses` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `doctor` FOREIGN KEY (`Doctor`) REFERENCES `doctors` (`FullName`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `patient` FOREIGN KEY (`Patient`) REFERENCES `patients` (`MedicalCardID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;s
--- Task10/Task10_Filling.sql
+++ Task10/Task10_Filling.sql
@@ -19,7 +19,14 @@ INSERT INTO `patients` (`MedicalCardID`, `FullName`, `DOB`, `Address`, `Gender`,
 	(3, 'Ivanova Maria Olegovna', '2001-11-03', 'Shkolnaya 81/1', 'Female', 0);
 
 
-INSERT INTO `appointments` (`AppointmentCard`, `Patient`, `Doctor`, `DateOfVisit`, `VisitPurpose`, `Diagnosis`, `Price`) VALUES
-	(1, 1, 'Kutayakov Dima Armanovich', '2022-02-11', 'Headache', 4, 3000.00),
-	(2, 3, 'Ivanov Ivan Denisovich', '2022-02-08', 'Broken arm', 6, 5000.00),
-	(3, 2, 'Petrov Petr Petrovich', '2022-02-10', 'Lost hearing', NULL, 1550.00);
+INSERT INTO `appointment_cards` (`ID`, `Doctor`, `Time`) VALUES
+	(1, 'Ivanov Ivan Denisovich', '9:00-9:30'),
+	(2, 'Ivanov Ivan Denisovich', '10:00-10:30'),
+	(3, 'Petrov Petr Petrovich', '14:10-14:40'),
+	(4, 'Kutayakov Dima Armanovich', '10:00-10:40');
+
+
+INSERT INTO `appointments` (`AppointmentCard`, `Patient`, `DateOfVisit`, `VisitPurpose`, `Diagnosis`, `Price`) VALUES
+	(1, 2, '2022-01-10', 'Broken arm', 6, 0.00),
+	(2, 3, '2022-02-10', 'Broken leg', 6, 0.00),
+	(4, 1, '2022-02-11', 'Cough', 5, 0.00);
