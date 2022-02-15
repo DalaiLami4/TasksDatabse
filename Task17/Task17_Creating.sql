5b54dc57b056a0d0cf1b6d1545e3ab5a06604669
DalaiLami4 <98631538+DalaiLami4@users.noreply.github.com>
2022-02-15 11:51:18 +0500

    Task17

---
+++ Task17/Task17_Creating.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE IF NOT EXISTS `task17`;
+USE `task17`;
+
+
+CREATE TABLE IF NOT EXISTS `employees` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `FullName` text NOT NULL,
+  `Position` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` text NOT NULL,
+  `Firm` text NOT NULL,
+  `Model` text NOT NULL,
+  `Specifications` text NOT NULL,
+  `GuaranteePeriod` int(11) NOT NULL,
+  `Picture` blob NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Customer` text NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `Guarantee` varchar(3) NOT NULL DEFAULT 'No',
+  `DateOfReceipt` date NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_orders_products` (`ProductID`),
+  CONSTRAINT `FK_orders_products` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `repairs` (
+  `EmployeeID` int(11) NOT NULL,
+  `OrderID` int(11) NOT NULL,
+  PRIMARY KEY (`EmployeeID`,`OrderID`),
+  KEY `FK__orders` (`OrderID`),
+  CONSTRAINT `FK__employees` FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK__orders` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `execution_of_orders` (
+  `OrderID` int(11) NOT NULL,
+  `EmployeeID` int(11) NOT NULL,
+  `TypeOfRepair` text NOT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  `DateOfExecution` date NOT NULL,
+  `ClientNotification` varchar(3) NOT NULL DEFAULT 'No',
+  `DateOfReceiving` date DEFAULT NULL,
+  `PaymentAmount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`OrderID`),
+  KEY `FK_execution_of_orders_employees` (`EmployeeID`),
+  CONSTRAINT `FK_execution_of_orders_employees` FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_execution_of_orders_orders` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
---
+++ Task17/Task17_Filling.sql
@@ -0,0 +1,29 @@
+INSERT INTO `employees` (`ID`, `FullName`, `Position`) VALUES
+	(1, 'Jack', 'Junior'),
+	(2, 'Masha', 'Senior'),
+	(3, 'Dima', 'Senior');
+
+
+INSERT INTO `products` (`ID`, `Name`, `Firm`, `Model`, `Specifications`, `GuaranteePeriod`, `Picture`) VALUES
+	(1, 'Iron', 'Bosh', 'DS343L', 'Yes', 0, _binary ''),
+	(2, 'Washing machine', 'Samsung', '4GH-3', 'Wash and Machine', 24, _binary ''),
+	(3, 'Vacuum cleaner', 'LG', 'DIN34', 'Cleaning', 15, _binary '');
+
+
+INSERT INTO `orders` (`ID`, `Customer`, `ProductID`, `Guarantee`, `DateOfReceipt`) VALUES
+	(1, 'Ivan', 1, 'No', '2022-01-01'),
+	(2, 'Olga', 3, 'Yes', '2022-01-20'),
+	(3, 'Sasha', 3, 'Yes', '2022-02-01');
+
+
+INSERT INTO `repairs` (`EmployeeID`, `OrderID`) VALUES
+	(1, 3),
+	(3, 1),
+	(3, 2),
+	(3, 3);
+
+
+INSERT INTO `execution_of_orders` (`OrderID`, `EmployeeID`, `TypeOfRepair`, `Price`, `DateOfExecution`, `ClientNotification`, `DateOfReceiving`, `PaymentAmount`) VALUES
+	(1, 3, 'Не утюжит', 1500.00, '2022-01-08', 'Yes', '2022-02-09', 1500.00),
+	(2, 3, 'Не сосет', 3650.00, '2022-01-29', 'Yes', '2022-02-02', 0.00),
+	(3, 1, 'Не включается', 3500.00, '2022-02-10', 'No', '2022-02-25', 0.00);
