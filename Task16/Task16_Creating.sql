b840421a54193cb8c89868f7caf51c156d6774f9
DalaiLami4 <98631538+DalaiLami4@users.noreply.github.com>
2022-02-17 14:09:19 +0500

    Corrected

--- Task16/Task16_Creating.sql
+++ Task16/Task16_Creating.sql
@@ -3,11 +3,11 @@ USE `task16`;
 
 
 CREATE TABLE IF NOT EXISTS `banks` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL,
   `Name` text NOT NULL,
   `LegalAddress` text NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `atms` (
@@ -21,24 +21,25 @@ CREATE TABLE IF NOT EXISTS `atms` (
 
 
 CREATE TABLE IF NOT EXISTS `customers` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CardNumber` int(11) NOT NULL,
   `FullName` text NOT NULL,
   `Address` text NOT NULL,
   `BankID` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
+  PRIMARY KEY (`CardNumber`) USING BTREE,
   KEY `FK_customers_banks` (`BankID`),
   CONSTRAINT `FK_customers_banks` FOREIGN KEY (`BankID`) REFERENCES `banks` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `operations` (
-  `CustomerID` int(11) NOT NULL,
+  `CardNumber` int(11) NOT NULL,
   `ATM_ID` int(11) NOT NULL,
   `DateAndTime` datetime NOT NULL,
   `Сommission` varchar(3) NOT NULL DEFAULT 'Нет',
   `Sum` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`CustomerID`,`ATM_ID`,`DateAndTime`) USING BTREE,
+  PRIMARY KEY (`CardNumber`,`ATM_ID`,`DateAndTime`) USING BTREE,
+  UNIQUE KEY `ATM_ID` (`ATM_ID`,`DateAndTime`),
   KEY `FK_operations_atms` (`ATM_ID`),
   CONSTRAINT `FK_operations_atms` FOREIGN KEY (`ATM_ID`) REFERENCES `atms` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_operations_customers` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `FK_operations_customers` FOREIGN KEY (`CardNumber`) REFERENCES `customers` (`CardNumber`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- Task16/Task16_Filling.sql
+++ Task16/Task16_Filling.sql
@@ -10,13 +10,13 @@ INSERT INTO `atms` (`ID`, `Address`, `BankID`) VALUES
 	(3, 'Short Street', 3);
 
 
-INSERT INTO `customers` (`ID`, `FullName`, `Address`, `BankID`) VALUES
+INSERT INTO `customers` (`CardNumber`, `FullName`, `Address`, `BankID`) VALUES
 	(1, 'Jack', 'Long Street', 2),
 	(2, 'John', 'Short Street', 1),
 	(3, 'Masha', 'Long Street', 3);
 
 
-INSERT INTO `operations` (`CustomerID`, `ATM_ID`, `DateAndTime`, `Сommission`, `Sum`) VALUES
+INSERT INTO `operations` (`CardNumber`, `ATM_ID`, `DateAndTime`, `Сommission`, `Sum`) VALUES
 	(1, 1, '2021-11-21 16:30:00', 'Да', 500.00),
 	(2, 1, '2021-12-13 18:00:00', 'Нет', 1500.00),
 	(3, 3, '2022-01-24 09:48:12', 'Нет', 150.00);
